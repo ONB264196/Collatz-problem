24c1a746d335befa4077c858a9d6c4aad00507fd
Harada Ikuru <bousi.264196.osouji@gmail.com>
2021-12-13 01:41:01 +0900

    プロジェクト ファイルを追加します。

---
+++ Collatz problem.cpp
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+int main(void) {
+	int i = 1;
+	int j = 0;
+	while (i < 500) {
+		j = i;
+		while (j != 1) {
+			if (j % 2 == 0) {
+				j /= 2;
+				printf("%5d", j);
+			}
+			else {
+				j = j * 3 + 1;
+				printf("%5d", j);
+			}
+		}
+		i++;
+		putchar('\n');
+	}
+	return 0;
+}
