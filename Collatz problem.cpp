1b3b62935b2a798385f90bb4f29efc26286b8058
Harada Ikuru <bousi.264196.osouji@gmail.com>
2021-12-13 01:49:01 +0900

    処理数の低減

--- Collatz problem.cpp
+++ Collatz problem.cpp
@@ -2,20 +2,25 @@
 int main(void) {
 	int i = 1;
 	int j = 0;
-	while (i < 500) {
+	do {
+		printf("n = %d	", i);
 		j = i;
 		while (j != 1) {
+			if (j < i) {
+				printf("「%d」", 1);
+				break;
+			}
 			if (j % 2 == 0) {
 				j /= 2;
-				printf("%5d", j);
+				printf("「%d」", j);
 			}
 			else {
 				j = j * 3 + 1;
-				printf("%5d", j);
+				printf("「%d」", j);
 			}
 		}
 		i++;
 		putchar('\n');
-	}
+	} while (i <= 5000);
 	return 0;
 }
